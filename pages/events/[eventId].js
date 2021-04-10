cf1e409857177879700979d8579ef100f0e24154
darrylmangibin <darrylmangibin@gmail.com>
2021-04-10 14:32:30 +0800

    Add event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,34 @@
+import { useRouter } from 'next/router';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistic from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
+import { getEventById } from '../../dummy-data';
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const { eventId } = router.query;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No Event found</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistic
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        alt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
 

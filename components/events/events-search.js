b9fd44306f78be0f880fd71fb68c9cce0720918d
darrylmangibin <darrylmangibin@gmail.com>
2021-04-10 15:16:45 +0800

    Add filter form for filtering events

---
+++ components/events/events-search.js
@@ -0,0 +1,47 @@
+import Button from '../ui/button';
+
+import classes from './event-search.module.css';
+
+function EventsSearch(props) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  return (
+    <form className={classes.form}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
+          <label htmlFor='year'>Year</label>
+          <select id='year'>
+            <option value='2021'>2021</option>
+            <option value='2022'>2022</option>
+          </select>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='month'>Month</label>
+          <select id='month'>
+            {months.map((month, i) => (
+              <option key={month} value={i + 1}>
+                {month}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <Button>Find Events</Button>
+    </form>
+  );
+}
+
+export default EventsSearch;
